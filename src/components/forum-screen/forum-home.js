2ab4b13daf4a47b2473d8ceb5947cefc81f661b1
richardcastaneda <61243707+richardcastaneda@users.noreply.github.com>
2021-04-25 17:13:43 -0700

    Updated Forum-Screen

--- src/components/forum-screen/forum-home.js
+++ src/components/forum-screen/forum-home.js
@@ -2,81 +2,46 @@ import React, {useState, useEffect} from 'react'
 import {Link} from "react-router-dom";
 import NavBar from "../navbar.js";
 import postService from "../../services/post-service"
-import userService from "../../services/user-service";
-import "../../styles/forum-page.css"
 
 const ForumScreen = () => {
-  const [currentUser, setCurrentUser] = useState({username: '', password: ''})
-  const [recentPost, setRecentPost] = useState([])
-  const [newPost, setNewPost] = useState({body: ""})
-  const [newTitle, setNewTitle] = useState({title: ""})
 
   useEffect(() => {
-    userService.profile()
-    .then((currentUser) => {
-      setCurrentUser(currentUser)
-    })
     findRecentPosts()
   }, [])
 
-  const createPost = () => {
-    postService.createPostForUser(currentUser.username, newPost)
-  }
+  const [recentPosts, setRecentPosts] = useState([])
+
 
   const findRecentPosts = () => {
     postService.findRecentPosts()
     .then((Results) => {
-      setRecentPost(Results);
-    })
+      setRecentPosts(Results);
+    }, [])
   }
 
   return(
       <>
-        <div className="container-fluid">
-          <div className="cryptocommunity-header">
+        <div class="container-fluid">
+          <div class="cryptocommunity-header">
             <h1>CryptoCommunity Forums</h1>
             <h4>Engage in Conversation and Learn More About Crypto</h4>
           </div>
           <NavBar/>
-          <h4>Recent Posts</h4>
           <div className="recent-posts">
             <ul className="list-group">
               {
-                recentPost.map((post) => {
+                recentPosts.map((post) => {
                   return(
-                    <li className="list-group-item-post">
-                      {post.title}
-                      {post.body}
-                      {post.author}
-                    </li>
+                      <li className="list-group-item">
+                        {post.title}
+                        {post.body}
+                        {post.author}
+                      </li>
                   )
                 })
               }
             </ul>
           </div>
-          <h4>Create a New Post</h4>
-          <div className="form-group">
-            <label htmlFor="titleInput">Title:</label>
-            <textarea
-                onChange={(event) => setNewTitle(event.target.value)}
-                className="form-control"
-                id="titleInput"
-                rows="1">
-            </textarea>
-          </div>
-          <div className="form-group">
-            <label htmlFor="bodyInput">Body:</label>
-            <textarea
-                onChange={(event) => setNewPost(event.target.value)}
-                className="form-control"
-                id="bodyInput"
-                rows="3"></textarea>
-          </div>
-          <button
-              onClick={createPost}
-              className="btn btn-success">
-            Create Post
-          </button>
           <div className="footer">
             <a href="https://www.privacypolicies.com/live/a9ccc0fc-fdec-4404-a260-4f009950b239">Privacy Policy</a>
             <p>Vincent Luo & Richard A. Castaneda <br/>
