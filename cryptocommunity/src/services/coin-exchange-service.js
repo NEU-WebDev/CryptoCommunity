445e567953126c9b816da187f01b2c0fc3f2a3b4
richardcastaneda <61243707+richardcastaneda@users.noreply.github.com>
2021-04-26 21:56:53 -0700

    Heroku server change

--- cryptocommunity/src/services/coin-exchange-service.js
+++ cryptocommunity/src/services/coin-exchange-service.js
@@ -1,20 +1,22 @@
+const key = "67FFC13C-2434-4FE5-886F-9F3C2F3D967F"
+
 const findExchangeRateByCoin = (coinCode) => {
-  return fetch(`https://rest.coinapi.io/v1/exchangerate/${coinCode}?apikey=AEDAA5E2-6289-4023-8457-60B011468684`)
+  return fetch(`https://rest.coinapi.io/v1/exchangerate/${coinCode}?apikey=${key}`)
   .then(response => response.json())
 }
 
 const findDetailedExchangeData = (coinCode, currencyCode) => {
-  return fetch(`https://rest.coinapi.io/v1/exchangerate/${coinCode}/${currencyCode}?apikey=AEDAA5E2-6289-4023-8457-60B011468684`)
+  return fetch(`https://rest.coinapi.io/v1/exchangerate/${coinCode}/${currencyCode}?apikey=${key}`)
     .then(response => response.json())
 }
 
 const getAllExchanges = () => {
-  return fetch(`https://rest.coinapi.io/v1/exchanges?apikey=AEDAA5E2-6289-4023-8457-60B011468684`)
+  return fetch(`https://rest.coinapi.io/v1/exchanges?apikey=${key}`)
   .then(response => response.json())
 }
 
 const findDetailedCoinData = (coinCode) => {
-  return fetch(`https://rest.coinapi.io/v1/assets/${coinCode}/?apikey=AEDAA5E2-6289-4023-8457-60B011468684`)
+  return fetch(`https://rest.coinapi.io/v1/assets/${coinCode}/?apikey=${key}`)
   .then(response => response.json())
 }
 
